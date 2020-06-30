255fb6ecbe43278bea68d619c4447a9b731770da
André Fettouhi <A.Fettouhi@gmail.com>
2020-06-30 09:18:51 +0200

    Small fixes to files in Chapter 1!

--- Chapter 1/NimTotals.R
+++ Chapter 1/NimTotals.R
@@ -6,19 +6,19 @@ install.packages("XLConnect")
 
 # The following group of commands is a script
 library(gmodels)  # required to use the CrossTable command
-library (XLConnect) # must have installed XLConnect
+library(XLConnect) # must have installed XLConnect
 Nimrod2 <- readWorksheetFromFile("/home/af/Dokumenter/Programs/GraphingDatawithR-R40/data/Nimrod.xls",sheet=1,
   header=TRUE)
 attach(Nimrod2)
-CrossTable(medium,level,
+CrossTable(Medium,Level,
   prop.r=FALSE,
   prop.c=FALSE,
   prop.t=FALSE,
   prop.chisq=FALSE)
 # above command prints table with counts in each cell,
 # but no percents
-perf_time <- summary(time)     # save summary output
-title  = "Summary of performance times:"
+perf_time <- summary(Time)     # save summary output
+title = "Summary of performance times:"
 cat(title,"\n", "\n")          # print title and 2 linefeeds
 print(perf_time)               # print results of summary(time)
 detach(Nimrod2)
--- Chapter 1/NimTotals2.R
+++ Chapter 1/NimTotals2.R
@@ -4,18 +4,18 @@ library(gmodels)
 library (XLConnect)
 Nimrod2<-readWorksheetFromFile(fn,sheet=1,header=TRUE)
 attach(Nimrod2)
-CrossTable(medium,level,
+CrossTable(Medium,Level,
   prop.r=FALSE,
   prop.c=FALSE,
   prop.t=FALSE,
   prop.chisq=FALSE)
  # print table with counts in each cell, but no percents
-perf_time = summary(time)  # save summary output
+perf_time = summary(Time)  # save summary output
 title  = "Summary of performance times:"
 cat(title,"\n", "\n")   # print title and 2 linefeeds
 print(perf_time)
 detach(Nimrod2)
 }
 
-update("myfile.xls")  # filename in quotes because myfile.xls
+update("/home/af/Dokumenter/Programs/GraphingDatawithR-R40/data/myfile.xls")  # filename in quotes because myfile.xls
                         # is the value of a character variable

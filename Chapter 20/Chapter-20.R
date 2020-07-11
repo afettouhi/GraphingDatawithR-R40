e60f8d9222949603aefa259350eb5d9dddbcb8a4
André Fettouhi <A.Fettouhi@gmail.com>
2020-07-11 09:17:24 +0200

    Fixed example code from Chapter 20!

--- Chapter 20/Chapter-20.R
+++ Chapter 20/Chapter-20.R
@@ -9,11 +9,12 @@ library(vcd)
 library(epiDisplay)
 library(vcd)
 data("ANCdata")
-xtab1 = table(ANCdata$death,ANCdata$anc)   # make this table an object, "xtab1"
+attach(ANCdata)
+xtab1 = table(death,anc)   # make this table an object, "xtab1"
 xtab1    # show the values in xtab1
 
-mosaicplot(xtab1) # command operates on table, not original cases
-mosaic(~ANCdata$death+ANCdata$anc)
+mosaic(xtab1) # command operates on table, not original cases
+mosaic(~death+anc)
 
 xtab1 = array(c(373, 46, 316, 20), c(2,2),
   list(c("no", "yes"), c("old", "new")))
@@ -27,7 +28,7 @@ anc = c("old", "new")
 xtab1 = array(counts, c(2,2), list(death, anc))
 names(dimnames(xtab1))  = c("death", "anc")
 xtab1           # prints Table 20-1
-mosaicplot(xtab1)   # produces Figure 20-1
+mosaic(xtab1)   # produces Figure 20-1
 
 # three-way table - data from Table 20-2
 library(vcd)
@@ -38,16 +39,16 @@ anc = c("old", "new")
 xtab2 = array(cnts, c(2,2,2), list(clinic, death, anc))
 names(dimnames(xtab2)) = c("clinic", "death", "anc")
 xtab2          # Table 20-2
-mosaicplot(xtab2)  # Figure 20-2
+mosaic(xtab2)  # Figure 20-2
 # use appropriate mosaic() command for any other plot in
 # chapter
 
 # Table 20-2
-xtab2 = table(ANCdata$clinic,ANCdata$death,ANCdata$anc)
+xtab2 = table(clinic,death,anc)
 xtab2 # produces the following table
 
 # Figure 20-2
-xtab2 = table(ANCdata$clinic,ANCdata$death,ANCdata$anc)
+xtab2 = table(clinic,death,anc)
 mosaic(xtab2)
 
 # Figure 20-3
@@ -55,14 +56,15 @@ mosaic(xtab2)
 mosaic(xtab2, highlighting = "anc")
 
 # Figure 20-4
-mosaic(table(ANCdata$clinic,ANCdata$anc,ANCdata$death),
+mosaic(table(clinic,anc,death),
   highlighting = 'death',
   highlighting_fill = c('royal blue', 'gold'))
 
 # Figure 20-5
-mosaic(table(ANCdata$death,ANCdata$anc,ANCdata$clinic),
+mosaic(table(death,anc,clinic),
   highlighting = 'clinic',
   highlighting_fill = c('royal blue', 'gold'))
 
 # Figure 20-6
 mosaic(xtab2, shade = TRUE, legend=TRUE)
+detach(ANCdata)
